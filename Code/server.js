ecc5f2df1311711bc3edeaf8c5f91d645373e9dd
Puriti275 <puriti275@gmail.com>
2025-11-17 12:19:42 -0500

    slight

---
+++ Code/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    console.log("here");
+    res.render('index');
+});
+
+app.get('/users', (req, res) => {
+    res.send("User List")
+})
+
+app.get('/users/new', (req, res) => {
+    res.send("User New Form")
+})
+
+app.listen(3000);
--- Scripts/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    console.log("here");
-    res.json({message: "hello world"});
-});
-
-app.listen(3000);
