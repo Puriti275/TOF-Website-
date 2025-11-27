22ffc9175cdd3ebfc466263c0e67080b4fbbda1a
Puriti275 <puriti275@gmail.com>
2025-11-27 01:33:42 -0600

    Fixed transparent button on website

--- Code/database.js
+++ Code/database.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: '127.0.0.1',
@@ -6,3 +6,8 @@ const pool = mysql.createPool({
     password: '#03222007Ae!',
     database: 'users_payments'
 }).promise()
+
+module.exports = pool;
+
+const result = pool.query("SELECT * FROM users")
+console.log(result)
