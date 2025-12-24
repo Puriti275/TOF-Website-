09562ce78ce2321dc26f0926c2a4abaf0c92f05f
Puriti275 <puriti275@gmail.com>
2025-12-24 16:32:21 -0600

    Update app.js
    made sure file was listening on the correct port with .env variables

--- Code/server/app.js
+++ Code/server/app.js
@@ -119,41 +119,9 @@ app.post('/create-checkout-session', async (req, res) => {
     }
 })
 
-/*
-app.post('/checkout-session-completed', async (req, res) => {
-    const session = req.body
-    const payment = await createPayment(session)
-    res.status(201)
-    console.log("Payment created")
-})
-*/
+const PORT = process.env.CLIENT_URL
 
-
-/*
-app.get("/users", async (req, res) => {
-    const users = await getUsers()
-    res.send(users)
-})
-
-app.get("/users/:id", async (req, res) => {
-    const id = req.params.id
-    const user = await getUser(id)
-    res.send(user)
-})
-
-app.post("/users", async (req, res) => {
-    const {firstName, lastName, email, city, address, zipcode } = req.body
-    const user = await createUser(firstName, lastName, email, city, address, zipcode)
-    res.status(201).send(user)
-})
-
-app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(500).send('Something broke!')
-})
-*/
-
-app.listen(8080, () => {
+app.listen(PORT, () => {
     console.log('Server is running on port 8080')
 })
 
